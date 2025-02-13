a4969c2bfb44c653974b7b6b447ba0d6f8e98c06
segni_abera <segniab69@gmail.com>
2025-02-13 23:04:16 +0300

    new files are added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,20 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
-    <WelcomeMessage />
+   
+     <WelcomeMessage />
 
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -32,6 +36,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Header />
+    <MainContent />
+    <Footer />
     </>
   )
 }

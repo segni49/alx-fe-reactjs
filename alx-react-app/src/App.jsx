7612f52062558b3d5215022365a030a7bf25770e
segni_abera <segniab69@gmail.com>
2025-02-13 23:17:13 +0300

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+
+
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -39,6 +42,7 @@ function App() {
       <Header />
     <MainContent />
     <Footer />
+    <UserProfile name= "Alice" age="25"  bio="Loves hiking and photography" />
     </>
   )
 }

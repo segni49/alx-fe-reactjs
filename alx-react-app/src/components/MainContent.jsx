461eb3871bd8cb700e7e6d95ba8305e4901cc9ee
segni_abera <segniab69@gmail.com>
2025-02-13 23:06:09 +0300

    file updated

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
-function Main () {
+function MainContent () {
     return (
         <main>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
     )
 }
-export default Main;
+export default MainContent;
